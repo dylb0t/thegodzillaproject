8f5e0d57eed48863c94c0a360c8f47923997e5b3
dylb0t <killer.bassist@gmail.com>
2021-09-15 16:26:45 -0700

    cleanup build and add some to readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # theproject
+To run a local http server w/ Pythion:
+```python3 -m http.server```
--- build/randomize.sh
+++ build/randomize.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-let pigNumber=0
-let bodyNumber=0
-let faceNumber=0
-let hatNumber=0
-let glassesNumber=0
-
-
-# Do functions with calls to header first and footer last
-# Can do blanks then
-# Can simply write the File Writing once, pass path
-
 # function to insert resources into the final gltf
 # expected argument is the path to the resource
 # expected local file named tempFile
@@ -55,11 +44,6 @@ for filename in $(ls ../webgl/models/hats); do
     let i=i+1
 done
 hats[i]="NONE"
-
-# for temp in ${filenames[@]}; do
-#     echo $temp
-# done
-
 
 
 # Every pig needs a body... So we will do the randomization for every single body
@@ -108,85 +92,5 @@ for filename in $(ls ../webgl/models/bodies); do
         done
     done
 
-    
-    #print the body to the tempfile
-    #insertresource $filename
-
-
-    #echo tempfile
 done
-exit
 
-for BODY in $(ls ../webgl/models/bodies/); do
-    # Do the main script insertion
-    #let pigNumber=$bodyNumber$faceNumber$hatNumber$glassesNumber
-
-    cat ./topfile > tempfile #../webgl/html/pig$pigNumber.html
-    # Do the body insertion
-    cat <<-EOF >> tempfile
-        loader.load( '/bodies/$BODY', function ( gltf ) {
-        gltf.scene.traverse( function ( child ) {
-            if ( child.isMesh ) {
-                child.castShadow = true; 
-            }
-        } );
-        gltf.scene.position.y += 9.4
-        scene.add( gltf.scene );
-        render();
-        } );
-EOF
-    #Do the face insertions
-    for FACE in $(ls ../webgl/models/face/); do
-        cat <<-EOF >> tempfile
-        loader.load( '/face/$FACE', function ( gltf ) {
-        gltf.scene.traverse( function ( child ) {
-            if ( child.isMesh ) {
-                child.castShadow = true; 
-            }
-        } );
-        gltf.scene.position.y += 9.4
-        scene.add( gltf.scene );
-        render();
-        } );
-EOF
-        #Do the hat insertions
-        for HAT in $(ls ../webgl/models/hats/); do
-            cat <<-EOF >> tempfile
-            loader.load( '/hats/$HAT', function ( gltf ) {
-            gltf.scene.traverse( function ( child ) {
-                if ( child.isMesh ) {
-                    child.castShadow = true; 
-                }
-            } );
-            gltf.scene.position.y += 9.4
-            scene.add( gltf.scene );
-            render();
-            } );
-EOF
-            #Do the glasses insertions
-            for GLASSES in $(ls ../webgl/models/glasses/); do
-                cat <<-EOF >> tempfile
-                loader.load( '/glasses/$GLASSES', function ( gltf ) {
-                gltf.scene.traverse( function ( child ) {
-                    if ( child.isMesh ) {
-                        child.castShadow = true; 
-                    }
-                } );
-                gltf.scene.position.y += 9.4
-                scene.add( gltf.scene );
-                render();
-                } );
-EOF
-                let pigNumber=$bodyNumber$faceNumber$hatNumber$glassesNumber
-                cat ./footerfile >> tempfile
-                cat ./tempfile > ../webgl/html/pig$pigNumber.html
-                let glassesNumber=$glassesNumber+1    
-            done
-            let hatNumber=$hatNumber+1
-        done
-        let faceNumber=$faceNumber+1
-    done
-
-    #cat ./footerfile >> ../webgl/html/pig$pigNumber.html
-    let bodyNumber=$bodyNumber+1
-done
